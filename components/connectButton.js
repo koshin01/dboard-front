41307575026cf9c86e8bbb8c157977a2202c866c
koshin <ekoshin2001@icloud.com>
2023-04-23 03:05:35 +0900

    読み込んだ時に、一瞬小さくなる問題を解決

--- components/connectButton.js
+++ components/connectButton.js
@@ -59,7 +59,7 @@ export default function ConnectButton() {
     return (
         <>
             {!account && (
-                <button onClick={connectWallet} className="text-white bg-slate-950 rounded-lg px-5 py-2.5">
+                <button onClick={connectWallet} className="w-96 text-white bg-slate-950 rounded-lg px-5 py-2.5">
                     Connect Wallet
                 </button>
             )}
